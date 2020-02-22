354a9c334e943957a425f584fd3f7d854f401253
JaySonani <jsonani98@gmail.com>
2020-02-22 19:08:04 +0530

    Base64 done

--- lib/screens/image_result.dart
+++ lib/screens/image_result.dart
@@ -2,86 +2,43 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 // import 'package:http/http.dart';
-// import 'dart:html';
 
 class ImageResult extends StatefulWidget {
-  final File imageInputFile; 
+  final File imageInputFile;
 
   ImageResult(this.imageInputFile);
-
-
 
   @override
   _ImageResultState createState() => _ImageResultState();
 }
 
 class _ImageResultState extends State<ImageResult> {
-
   final String url = "https://www.google.com/";
   // String classId, className;
 
   var bytes;
-  String base_64, encoded;
-
-
-  
+  String base_64;
+  var file;
 
   @override
-  void initState()  { 
+  void initState() {
     super.initState();
 
     bytes = widget.imageInputFile.readAsBytesSync();
-    base_64 = base64.encode(bytes);
-    encoded = base64Encode(bytes);
-
+    base_64 = base64Encode(bytes);
+    print(base_64);
 
     // this.getJsonData();
     //Call API here
   }
 
-  Future<String> getJsonData() async{
-    var response = await http.get(url);
-    print(response.body);
-
-    setState(() {
-      // var convertDataToJson = jsonDecode(response.body);
-      // classId = convertDataToJson['class_id'];
-      // className = convertDataToJson['class_name'];
-    });
-    return "success";
-
-  }
-
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 200,
-          width: 300,
-          child: Image.file(widget.imageInputFile),
-        ),
+        child: Text("label"),
       ),
     );
-
   }
-
-
-
-
-
-
-
-
-
-
 }
-
-
