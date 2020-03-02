4ed46f4bbf81a91f55c24a867dda159f185bbf32
JaySonani <jsonani98@gmail.com>
2020-03-02 14:17:41 +0530

    Integrated initiated

--- lib/screens/image_result.dart
+++ lib/screens/image_result.dart
@@ -1,11 +1,20 @@
 // import 'dart:convert';
 // import 'dart:convert';
 import 'dart:io';
+// import 'package:aibirdie/screens/img_classification.dart';
 import 'package:flutter/material.dart';
 // import 'package:http/http.dart';
 
+import './aibirdie_image_api/aibirdie_image_classification.dart';
+import './aibirdie_image_api/generated/image_classification.pb.dart';
+
+// import 'package:http/http.dart';
+
+
+
 class ImageResult extends StatefulWidget {
   final File imageInputFile;
+  
 
   ImageResult(this.imageInputFile);
 
@@ -14,20 +23,30 @@ class ImageResult extends StatefulWidget {
 }
 
 class _ImageResultState extends State<ImageResult> {
+
+  AiBirdieImageClassification classifier;
+  var response;
+  
+
   @override
   void initState() {
     super.initState();
 
+    // classifier = AiBirdieImageClassification('127.0.0.1');
+    // classifier.predict(widget.imageInputFile.path).then((value){
+      
+    //   print('Response: $response');
 
-    // this.getJsonData();
-    //Call API here
+    // });
+    
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("label"),
+        child: Text("$response"),
       ),
     );
   }
